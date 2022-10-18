48c1910bb757cc9975d1eba2280a14452f32f9d2
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2022-10-18 13:01:19 -0500

    Ultimos cambios en agregar categoria

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -6,7 +6,6 @@ export const AddCategory = () => {
     const [inputValue, setInputValue] = useState('One punch');
 
     const onInputChange = ({ target }) => {
-        console.log(target.value)
         setInputValue(target.value);
     }
     
