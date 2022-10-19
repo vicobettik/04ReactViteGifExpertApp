d22f6f6e5a610613b016d61577d8af27aef5c0ba
Victor Fuentes <amaury.fuentes@iisi.com.mx>
2022-10-19 18:17:54 -0500

    se agrega helper para fetch de gifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,21 +1,9 @@
 import React from 'react'
+import { getGifs } from '../helpers/getGifs'
 
 export const GifGrid = ({category}) => {
 
-    const getGifs = async() => {
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=Odlm70jt3CcrWhTplchwKFSPNS80Vo7b&q=${category}&limit=20`;
-        const resp = await fetch( url );
-        const {data} = await resp.json();
-
-        const gifs = data.map( (img) => ({
-            id: img.id,
-            title: img.title,
-            url: img.images.downsized_medium.url
-        }))
-        console.log(gifs);
-    }
-
-    getGifs();
+  getGifs(category);
 
   return (
     <>
