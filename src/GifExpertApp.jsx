48a2c9d993529d55065d61bfde6c44d1a1041e77
Victor Fuentes <amaury.fuentes@iisi.com.mx>
2022-10-18 23:51:22 -0500

    se agrega componente gifGrid, se realiza peticion a la api de giphy

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,35 +1,38 @@
 import React, { useState } from 'react'
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
-  const [categories, setCategories] = useState(['one puch', 'dragon ball']);
+  const [categories, setCategories] = useState(['one puch']);
 
   const onAddCategory = ( newCategory ) => {
     console.log(newCategory)
+    if ( categories.includes(newCategory) ) {
+      return;
+    }
     // setCategories([newCategory, ...categories]);
     setCategories( (cat) =>  [newCategory, ...cat]);
   };
 
   return (
     <>
-        {/* titulo */}
         <h1>Gif expert app</h1>
 
-        {/* input  */}
         <AddCategory 
           // setCategories={ setCategories }
           onNewCategory={ (event) => onAddCategory(event) }
         />
 
-        {/* listado de gifs */}
-        <ol>
-          {
-            categories.map((cat) => {
-              return <li key={cat}>{cat}</li>
-            })
-          }
-        </ol>
+        {
+          categories.map((category) => (
+              <GifGrid 
+                key={ category }
+                category={ category }
+              />
+            )
+          )
+        }
           {/* gif item */}
     </>
   )
