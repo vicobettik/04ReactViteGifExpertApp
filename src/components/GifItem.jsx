fd58c50317a49f117d74e9cbe77af0dcd75390f8
Victor Fuentes <amaury.fuentes@iisi.com.mx>
2022-10-20 11:42:52 -0500

    se crea hook personalizado para obtener gifs

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export const GifItem = ({ title, url, id }) => {
-  console.log({title, url, id});
   return (
     <div className="card">
         <img src={ url } alt={ title } />
