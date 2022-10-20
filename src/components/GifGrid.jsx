e3252df88ee718e26a4a57b87f8ecd5951546bab
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2022-10-19 22:19:58 -0500

    se agrega use effect y se obtienen las imagenes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { getGifs } from '../helpers/getGifs'
 
 export const GifGrid = ({category}) => {
 
-  getGifs(category);
+  const [images, setImages] = useState([]);
+
+  const getImages = async() => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  }
+
+  useEffect( () => {
+    getImages();
+  }, [] );
+
 
   return (
     <>
       <h3>{ category }</h3>
+      <ol>
+        {
+          images.map( ({id, title}) => (
+            <li key={id}>{title}</li>
+          ))
+        }
+      </ol>
     </>
   )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=Odlm70jt3CcrWhTplchwKFSPNS80Vo7b&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=Odlm70jt3CcrWhTplchwKFSPNS80Vo7b&q=${category}&limit=10`;
     const resp = await fetch( url );
     const {data} = await resp.json();
 
