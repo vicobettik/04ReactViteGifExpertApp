e56e24a0197dc830d1b5cc238253f3b82a133945
Victor Fuentes <amaury.fuentes@iisi.com.mx>
2022-10-20 10:42:28 -0500

    se crea componente de item del grid de gifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getGifs } from '../helpers/getGifs'
+import { GifItem } from './GifItem';
 
 export const GifGrid = ({category}) => {
 
@@ -18,13 +19,16 @@ export const GifGrid = ({category}) => {
   return (
     <>
       <h3>{ category }</h3>
-      <ol>
+      <div className='card-grid'>
         {
-          images.map( ({id, title}) => (
-            <li key={id}>{title}</li>
+          images.map( (image) => (
+            <GifItem 
+              key={ image.id }
+              { ...image }
+            />
           ))
         }
-      </ol>
+      </div>
     </>
   )
 }
